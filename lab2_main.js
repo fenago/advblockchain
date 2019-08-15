4f415b477b19c8423c59006e61f7c86c069d6843
fenago <socrates73@gmail.com>
2019-08-15 07:27:00 -0400

    Update lab2_main.js

--- lab2_main.js
+++ lab2_main.js
@@ -3,11 +3,9 @@
   A. create an empty method called createHash
   (This method will take the properties of the block and 
   run it through a hash method)
-  B. go to your terminal and install 
-  crypto-js with this syntax: npm install --save crypto-js
-  make sure it is done from the ErnestoCoin directory and with bash
-  C. you can confirm that it is installed by looking in /home/lab/node_modules for crypto-js 
-  D. import sha 256 (i.e.: const SHA256 = require('../node_modules/crypto-js/sha256');
+  B. in repl.it   --- select packages and pull in crypto-js
+  C. you can confirm that it is installed by looking for a file named package.json in the same directory 
+  D. import sha 256 (i.e.: const SHA256 = require('crypto-js/sha256');
   E. In your createHash method - hash all of the variables brought in from the constructor
   F. Finally, update your hash local variable by calling the createHash method 
   */
