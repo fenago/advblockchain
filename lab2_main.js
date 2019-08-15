7f3f61fadf1d3003ccd77245fdf3b956ca012664
fenago <socrates73@gmail.com>
2019-08-15 07:27:29 -0400

    Update lab2_main.js

--- lab2_main.js
+++ lab2_main.js
@@ -3,7 +3,7 @@
   A. create an empty method called createHash
   (This method will take the properties of the block and 
   run it through a hash method)
-  B. in repl.it   --- select packages and pull in crypto-js
+  B. in repl.it  (MAKE SURE YOU ARE USING nodejs version of repl.it) --- select packages and pull in crypto-js
   C. you can confirm that it is installed by looking for a file named package.json in the same directory 
   D. import sha 256 (i.e.: const SHA256 = require('crypto-js/sha256');
   E. In your createHash method - hash all of the variables brought in from the constructor
