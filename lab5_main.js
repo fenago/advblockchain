0b6a2de2d435cfe693268804cfdebd2111494f8a
fenago <socrates73@gmail.com>
2020-07-13 23:05:05 -0400

    Update lab5_main.js

--- lab5_main.js
+++ lab5_main.js
@@ -6,8 +6,16 @@ C. Check to see if the current block hash is equal to the calculated hash of the
 D. Check to see if the block points to the right prior hash.  If not- return false
 E. Comment out the existing console.log and add:  
 console.log('Is Ernesto Blockchain Valid? ' = ErnestoCoin.isBCValid() and run it with node main.js
-F. from the CLI -- ErnestoCoin.chain[1].data = 'amount = 1,000'  // this tampers with the chain.  run node main.js
-G. from the CLI -- ErnestoCoin.chain[1].hash = ErnestoCoin.chain[1].createHash()  // this updates the hash so you have updated the data and then updated the hash -- run node main.js
+F. from the CLI -- 
+  ErnestoCoin.chain[1].data = 'amount = 1,000'  // this tampers with the chain.  run node main.js
+  JSON.stringify(ErnestoCoin, null, 4))
+  ErnestoCoin.isBCValid()
+G. from the CLI -- 
+  ErnestoCoin.chain[1].createHash()
+  JSON.stringify(ErnestoCoin, null, 4))
+  ErnestoCoin.isBCValid()
+  
+  ErnestoCoin.chain[1].hash = ErnestoCoin.chain[1].createHash()  // this updates the hash so you have updated the data and then updated the hash -- run node main.js
 H. Obviously if you notice that the blockchain is not valid, you should roll back the changes and put your blockchain back into it's appropriate state.  Add this feature.  Notice that this 
 blockchain does not have a proof of work algorithm, it does not execute on a distributed network, it does not have a currency.
 
